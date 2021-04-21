6eaca72cf85e6245d5f35bd06c10e0ba660ca576
Irwandi Refraugati <65169347+irwandirefraugati@users.noreply.github.com>
2021-04-21 19:26:40 +0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Irwand
+ */
+public class Hello 
+{
+    /**
+     * My first java program
+     */
+    public static void main(String[] args) {
+    
+        //Menampilkan kata "Hello world!" dilayar
+            System.out.println("Hello world!");
+    
+    }
+}
