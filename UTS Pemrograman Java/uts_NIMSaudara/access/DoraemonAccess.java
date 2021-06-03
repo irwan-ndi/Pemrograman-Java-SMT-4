3d6e8e61d11bb778ed1eb1a78f51c88e829b55a7
Irwandi Refraugati <65169347+irwandirefraugati@users.noreply.github.com>
2021-06-03 23:54:25 +0700

    Create DoraemonAccess.java

---
+++ UTS Pemrograman Java/uts_NIMSaudara/access/DoraemonAccess.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utama;
+
+/**
+ *
+ * @author Irwand
+ */
+public class Doraemon {
+    
+    private String warna;
+    private int kumis;
+    
+    protected String setWarna (String newWarna){
+        return this.warna = newWarna;
+    }
+    
+    public void getWarna(){
+        System.out.println("warna doraemon"+warna);
+    }
+    
+    protected int setKumis(int newKumis){
+        return this.kumis = newKumis;
+    }
+    
+    public void getKumis(){
+        System.out.println("jumlah kumis doraemon "+kumis);
+    }
+    
+}
