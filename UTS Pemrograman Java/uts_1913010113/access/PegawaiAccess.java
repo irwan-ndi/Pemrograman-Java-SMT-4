f42851d50b1f120b1187883a811919af2c22bf85
Irwandi Refraugati <65169347+irwandirefraugati@users.noreply.github.com>
2021-06-04 00:20:36 +0700

    Update PegawaiAccess.java

--- UTS Pemrograman Java/uts_1913010113/access/PegawaiAccess.java
+++ UTS Pemrograman Java/uts_1913010113/access/PegawaiAccess.java
@@ -1,1 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package access;
+
+import Utama.Doraemon;
+
+/**
+ *
+ * @author Irwand
+ */
+public class DoraemonAccess extends Doraemon{
+    public static void main(String[] args) {
+            DoraemonAccess dora = new DoraemonAccess();
+        dora.setWarna("Biru");
+        dora.getWarna();
+        dora.setKumis(6);
+        dora.getKumis();
+    }
+}
 
