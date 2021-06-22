82501ac1d2eddca4f865023eca6360a7a62edd0a
Irwandi Refraugati <irwandirefraugati@gmail.com>
2021-06-22 22:51:51 +0700

    Add files via upload

---
+++ Praktikum 06 - Constructor/6.2. PARAMETERIZED CONSTRUCTOR/ConstructorAccess.java
@@ -0,0 +1,8 @@
+public class ConstructorAccess {
+    public static void main(String[] args) {
+        ParamConstructor pc = new
+              ParamConstructor("Luffy", 3, "L2001500xy");
+        pc.info();
+    }
+  }
+  
