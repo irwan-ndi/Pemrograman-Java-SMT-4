58d290304fdbe71e8d2baca8c15780c2b1322b30
Irwandi Refraugati <irwandirefraugati@gmail.com>
2021-06-23 04:43:57 +0700

    Add files via upload

---
+++ Praktikum 06 - Constructor/6.2. PARAMETERIZED CONSTRUCTOR/6.2.1. Latihan 2/2/BukuAccess.java
@@ -0,0 +1,25 @@
+public class BukuAccess {
+    public static void main(String[] args) {
+        // yang dikurung merupakan parameter
+        Buku buku1 = new Buku ("Ahmad Fuadi", "Negeri 5 Menara", 2009, 1000, 40000);
+        buku1.info();
+        Buku buku2 = new Buku ("Pramoedya Ananta Toer", "Bumi Manusia", 1980, 900, 120000);
+        buku2.info();
+        Buku buku3 = new Buku ("Ahmad Tohari", "Ronggeng Dukuh Paruk", 1982, 800, 20000);
+        buku3.info();
+        Buku buku4 = new Buku ("Marchella FP", "Nanti Kita Cerita tentang Hari Ini", 2018, 700, 40000);
+        buku4.info();
+        Buku buku5 = new Buku ("Andrea Hirata", "Sang Pemimpi", 2006, 600, 50000);
+        buku5.info();
+        Buku buku6 = new Buku ("Pramoedya Ananta Toer", "Anak Semua Bangsa", 1980, 500, 25000);
+        buku6.info();
+        Buku buku7 = new Buku ("Marchella FP", "Kamu Terlalu Banyak Bercanda", 2019, 400, 125000);
+        buku7.info();
+        Buku buku8 = new Buku ("Pramoedya Ananta Toer", "Jejak Langkah", 1985, 300, 130000);
+        buku8.info();
+        Buku buku9 = new Buku ("Andrea Hirata", "Edensor", 2007, 200, 30000);
+        buku9.info();
+        Buku buku10 = new Buku ("Eka Kurniawan", "Cantik itu Luka", 2002, 100, 125000);
+        buku10.info();
+    }
+  }
