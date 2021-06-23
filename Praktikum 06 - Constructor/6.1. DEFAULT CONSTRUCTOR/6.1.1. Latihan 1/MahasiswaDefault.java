7354f6d9208912186630b266ac7fa72558982fba
Irwandi Refraugati <irwandirefraugati@gmail.com>
2021-06-23 21:17:36 +0700

    Add files via upload

---
+++ Praktikum 06 - Constructor/6.1. DEFAULT CONSTRUCTOR/6.1.1. Latihan 1/MahasiswaDefault.java
@@ -0,0 +1,10 @@
+public class Mahasiswa {
+    String nama = "Irwandi Refraugati";
+    int nim = 1913010113; 
+    String alamat = "Bayan, Kadipiro, Surakarta";
+
+    void display() {
+        System.out.println("Nama : " + nama + "\nNIM : "
+                  + nim +"\nAlamat : " + alamat);
+    }
+  }
---
+++ Praktikum 06 - Constructor/6.1. DEFAULT CONSTRUCTOR/6.1.1. Latihan 1/MahasiswaDefaultAccess.java
@@ -0,0 +1,7 @@
+public class MahasiswaAccess {
+    public static void main(String[] args) {
+        Mahasiswa m = new Mahasiswa();
+
+        m.display();
+    }
+  }
